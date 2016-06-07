64ac86e5f3c67adfcdf524ce7fc0d3ef26aa53dd
Sébastien Cayer <sebastien.cayer@consultationsc2.com>
2016-06-07 15:19:48 -0400

    Fix AECredentialsTest

--- src/test/java/com/automic/AECredentialsTest.java
+++ src/test/java/com/automic/AECredentialsTest.java
@@ -1,46 +1,105 @@
 package com.automic;
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AECredentialsTest {
 
-	@Test(expected = IllegalArgumentException.class)
-	public void testAEHostnameOrIpEmpty() {
-		new AECredentials("", 0, 0, "", "", "", 'E');
+	/**
+	 * Logger.
+	 */
+	private final Logger LOGGER = LoggerFactory.getLogger(getClass());
+
+	@Test
+	public void aeHostnameOrIpEmptyTest() {
+		try {
+			new AECredentials("", 0, 0, "", "", "", 'E');
+			fail("This test should throw IllegalArgumentException");
+		}
+		catch (IllegalArgumentException iae) {
+			LOGGER.info(iae.getMessage(), iae);
+			assertTrue(iae.getMessage().indexOf("AEHostnameOrIp") != -1);
+		}
+
 	}
 
-	@Test(expected = IllegalArgumentException.class)
-	public void testAECPPort() {
-		new AECredentials("test", 0, 0, "", "", "", 'E');
+	@Test
+	public void aeCPPortTest() {
+		try {
+			new AECredentials("test", 0, 0, "", "", "", 'E');
+		}
+		catch (IllegalArgumentException iae) {
+			LOGGER.info(iae.getMessage(), iae);
+			assertTrue(iae.getMessage().indexOf("AECPPort") != -1);
+		}
 	}
 
-	@Test(expected = IllegalArgumentException.class)
-	public void testAEClientToConnect() {
-		new AECredentials("test", 123, -1, "", "", "", 'E');
+	@Test
+	public void aeClientToConnectTest() {
+		try {
+			new AECredentials("test", 123, -1, "", "", "", 'E');
+		}
+		catch (IllegalArgumentException iae) {
+			LOGGER.info(iae.getMessage(), iae);
+			assertTrue(iae.getMessage().indexOf("AEClientToConnect") != -1);
+		}
 	}
 
-	@Test(expected = IllegalArgumentException.class)
-	public void testAEClientToConnectGranterThan() {
-		new AECredentials("test", 123, 10000, "", "", "", 'E');
+	@Test
+	public void aeClientToConnectGranterThanTest() {
+		try {
+			new AECredentials("test", 123, 10000, "", "", "", 'E');
+		}
+		catch (IllegalArgumentException iae) {
+			LOGGER.info(iae.getMessage(), iae);
+			assertTrue(iae.getMessage().indexOf("AEClientToConnect") != -1);
+		}
 	}
 
-	@Test(expected = IllegalArgumentException.class)
-	public void testAEUserLogin() {
-		new AECredentials("test", 123, 123, "", "", "", 'E');
+	@Test
+	public void aeUserLoginTest() {
+		try {
+			new AECredentials("test", 123, 123, "", "", "", 'E');
+		}
+		catch (IllegalArgumentException iae) {
+			LOGGER.info(iae.getMessage(), iae);
+			assertTrue(iae.getMessage().indexOf("AEUserLogin") != -1);
+		}
 	}
 
-	@Test(expected = IllegalArgumentException.class)
-	public void testAEDepartment() {
-		new AECredentials("test", 123, 123, "ccc", "", "", 'E');
+	@Test
+	public void aeDepartmentTest() {
+		try {
+			new AECredentials("test", 123, 123, "ccc", "", "", 'E');
+		}
+		catch (IllegalArgumentException iae) {
+			LOGGER.info(iae.getMessage(), iae);
+			assertTrue(iae.getMessage().indexOf("AEDepartment") != -1);
+		}
 	}
 
-	@Test(expected = IllegalArgumentException.class)
-	public void testAEUserPassword() {
-		new AECredentials("test", 123, 123, "ccc", "asdasd", "", 'E');
+	@Test
+	public void aeUserPasswordTest() {
+		try {
+			new AECredentials("test", 123, 123, "ccc", "asdasd", "", 'E');
+		}
+		catch (IllegalArgumentException iae) {
+			LOGGER.info(iae.getMessage(), iae);
+			assertTrue(iae.getMessage().indexOf("AEUserPassword") != -1);
+		}
 	}
 
-	@Test(expected = IllegalArgumentException.class)
-	public void AEMessageLanguage() {
-		new AECredentials("test", 123, 123, "ccc", "asdasd", "", 'e');
+	@Test
+	public void aeMessageLanguageTest() {
+		try {
+			new AECredentials("test", 123, 123, "ccc", "asdasd", "asdasd", 'e');
+		}
+		catch (IllegalArgumentException iae) {
+			LOGGER.info(iae.getMessage(), iae);
+			assertTrue(iae.getMessage().indexOf("AEMessageLanguage") != -1);
+		}
 	}
 }
